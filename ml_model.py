13c2bf6a8a95418729f8bd76556cd3b5239f5c97
biswasamanta <biswasamanta@gmail.com>
2022-07-31 13:32:28 -0700

    added 3 new methods

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,14 @@
 print("Addinf all ML related code here")
+
+def model1():
+    print("Adding code fir ML1")
+
+def model2():
+    print("Adding code fir ML2")
+
+def model3():
+    print("Adding code fir ML3")
+
+
+
+print("model2 is performing best")
